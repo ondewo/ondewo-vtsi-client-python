94f9380bef07cdc6363deae99e9a356532e9d954
fergus <fergusfettes@gmail.com>
2021-02-19 16:27:19 +0100

    added license header to files

--- ondewo/vtsi/utils.py
+++ ondewo/vtsi/utils.py
@@ -1,3 +1,14 @@
+# Copyright 2021 ONDEWO GmbH
+# Licensed under the ONDEWO GmbH license, Version 1.0 (the "License");
+# you must not use this file except in compliance with the License.
+# You must obtain a copy of the License at
+# office@ondewo.com
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import uuid
 from typing import List
 
