4e110c75cea587bdb682d29e6c7b2cfb64575357
akantor <attila.kantor@ondewo.com>
2021-06-10 14:44:31 +0200

    Typo

--- ondewo/vtsi/client.py
+++ ondewo/vtsi/client.py
@@ -277,7 +277,7 @@ class VtsiClient:
                     sip_prefix=sip_prefix,
                     password_dictionary=password_dictionary,
                 )
-                print(f"start caller request added to {sip_name}")
+                print(f"start caller request added for sip_name {sip_name}")
             else:
                 request = CallConfig.get_call_proto_request(
                     manager=self.manager,
@@ -289,7 +289,7 @@ class VtsiClient:
                     contexts=contexts or self.manager.config_cai.cai_contexts,
                     sip_name=sip_name,
                 )
-                print(f"start listener request added to {sip_name}")
+                print(f"start listener request added for sip_name {sip_name}")
             call_request_list.append(request)
 
         print("performing multiple calls")
