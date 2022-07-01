4c8e58688d6729c3181e6b2dba20f6e8f68a32b3
Mohamed Masalati <mohamed.masalati@ondewo.com>
2022-07-01 17:02:55 +0200

    update client

--- ondewo/vtsi/client.py
+++ ondewo/vtsi/client.py
@@ -12,36 +12,30 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import grpc
 import os
 from dataclasses import dataclass
 from typing import List, Optional, Dict, Any
 
-import grpc
-
-from ondewo.nlu import context_pb2
-from ondewo.vtsi import call_log_pb2, call_log_pb2_grpc, voip_pb2, voip_pb2_grpc
+from ondewo.vtsi import voip_pb2, voip_pb2_grpc
 
 
 @dataclass
 class VtsiConfiguration:
-    """location of voip server"""
+    """
+        Vtsi Configurations
+        * host : Host of VTSI server
+        * port: Port of VTSI server
+        * secure: whether the server should be secure or not
+        You only need one of the following
+        * grpc_cert: GRPC cert
+        * cert_path: GRPC cert path
+    """
     host: str = "grpc-vtsi.ondewo.com"
     port: int = 443
     secure: bool = False
+    grpc_cert: str = ''
     cert_path: str = ''
-
-
-def create_parameter_dict(my_dict: Dict) -> Optional[Dict[str, context_pb2.Context.Parameter]]:
-    assert isinstance(my_dict, dict) or my_dict is None, "parameter must be a dict or None"
-    if my_dict is not None:
-        return {
-            key: context_pb2.Context.Parameter(
-                display_name=key,
-                value=my_dict[key]
-            )
-            for key in my_dict
-        }
-    return None
 
 
 class VtsiClient:
@@ -59,7 +53,9 @@ class VtsiClient:
         # create grpc service stub
         if self.config_voip.secure:
 
-            if os.path.exists(self.config_voip.cert_path):
+            if self.config_voip.grpc_cert:
+                grpc_cert = self.config_voip.grpc_cert
+            elif os.path.exists(self.config_voip.cert_path):
                 with open(self.config_voip.cert_path, "rb") as fi:
                     grpc_cert = fi.read()
 
@@ -70,111 +66,87 @@ class VtsiClient:
             channel = grpc.insecure_channel(target=target)
             print(f'Creating an insecure channel to {target}')
         self.voip_stub = voip_pb2_grpc.VoipSessionsStub(channel=channel)
-        self.call_log_stub = call_log_pb2_grpc.VoipCallLogsStub(channel=channel)
 
-    def stop_call(
-            self, call_id: Optional[str] = None, sip_id: Optional[str] = None,
-    ) -> bool:
-        """
-        stop an ongoing call
-        """
-        return self._stop_call(call_id=call_id, sip_id=sip_id)
+    def start_listeners(
+            self,
+            request: StartListenersRequest,
+            context: grpc.ServicerContext,
+    ) -> StartListenersResponse:
+        return self.voip_stub.StartListeners(request=request)
 
-    def _stop_call(
-            self, call_id: Optional[str] = None, sip_id: Optional[str] = None,
-    ) -> bool:
-        """
-        stop a call instance
-        """
-        if call_id:
-            request = voip_pb2.StopCallInstanceRequest(call_id=call_id)
-        elif sip_id:
-            request = voip_pb2.StopCallInstanceRequest(sip_id=sip_id)
-        else:
-            raise ValueError("either call_id or sip_id needs to be specified!")
-        print("stopping call")
-        response: voip_pb2.StopCallInstanceResponse = self.voip_stub.StopCallInstance(request=request)
-        return response.success  # type: ignore
+    def start_callers(
+            self,
+            request: StartCallersRequest,
+            context: grpc.ServicerContext,
+    ) -> StartCallersResponse:
+        return self.voip_stub.StartCallers(request=request)
 
-    def start_call_instance_request(self, start_call_instance_requests: voip_pb2.StartCallInstanceRequest
-                                    ) -> voip_pb2.StartCallInstanceResponse:
+    def start_scheduled_callers(
+            self,
+            request: StartScheduledCallersRequest,
+            context: grpc.ServicerContext,
+    ) -> StartScheduledCallersResponse:
+        return self.voip_stub.StartScheduledCallers(request=request)
 
-        return self.voip_stub.StartCallInstance(request=start_call_instance_requests)
+    def stop_calls(
+            self,
+            request: StopCallsRequest,
+            context: grpc.ServicerContext,
+    ) -> StopCallsResponse:
+        return self.voip_stub.StopCalls(request=request)
 
-    def start_multiple_call_instances(self,
-                                      start_call_instance_requests: voip_pb2.StartMultipleCallInstancesRequest):
-        return self.voip_stub.StartMultipleCallInstances(request=start_call_instance_requests)
+    def stop_all_calls(
+            self,
+            request: empty_pb2.Empty,
+            context: grpc.ServicerContext,
+    ) -> StopAllCallsResponse:
+        return self.voip_stub.StopAllCalls(request=request)
 
-    def get_instance_status(self, call_id: Optional[str] = None,
-                            sip_id: Optional[str] = None, ) -> voip_pb2.VoipStatus:
-        """
-        stop a listener instance
-        """
-        if call_id:
-            request = voip_pb2.GetVoipStatusRequest(call_id=call_id)
-        elif sip_id:
-            request = voip_pb2.GetVoipStatusRequest(sip_id=sip_id)
-        else:
-            raise ValueError("either call_id or sip_id needs to be specified!")
-        print("getting instance status")
-        response: voip_pb2.VoipStatus = self.voip_stub.GetInstanceStatus(request=request)
-        return response
+    def transfer_call(
+            self,
+            request: TransferCallRequest,
+            context: grpc.ServicerContext,
+    ) -> TransferCallResponse:
+        return self.voip_stub.TransferCall(request=request)
 
-    def update_services_status(
-            self, call_id: Optional[str] = None, sip_id: Optional[str] = None,
-            manifest_id: Optional[str] = None,
-    ) -> voip_pb2.UpdateServicesStatusResponse:
-        """
-        send update requests to speech-to-text, asterisk, text-to-speech and cai, which can then be retrieved by
-            get_instance_status() or
-            get_manifest_status()
-        """
-        if call_id:
-            request = voip_pb2.UpdateServicesStatusRequest(call_id=call_id)
-        elif sip_id:
-            request = voip_pb2.UpdateServicesStatusRequest(sip_id=sip_id)
-        elif manifest_id:
-            request = voip_pb2.UpdateServicesStatusRequest(manifest_id=manifest_id)
-        else:
-            raise ValueError("either call_id, sip_id or manifest_id needs to be specified!")
-        print("updating services' status")
-        response: voip_pb2.UpdateServicesStatusResponse = self.voip_stub.UpdateServicesStatus(request=request)
-        return response
+    def get_voip_call_info(
+            self,
+            request: GetVoipCallInfoRequest,
+            context: grpc.ServicerContext,
+    ) -> GetVoipCallInfoResponse:
+        return self.voip_stub.GetVoipCallInfo(request=request)
 
-    def get_call_ids(self) -> List[str]:
-        """
-        get all call_ids known to the voip manager
-        """
-        request = voip_pb2.GetCallIDsRequest()
-        response: voip_pb2.GetCallIDsResponse = self.voip_stub.GetCallIDs(request=request)
-        call_ids = []
-        for call_id in response.call_ids:
-            call_ids.append(call_id)
-        return call_ids
+    def list_voip_call_info(
+            self,
+            request: ListVoipCallInfoRequest,
+            context: grpc.ServicerContext,
+    ) -> ListVoipCallInfoResponse:
+        return self.voip_stub.ListVoipCallInfo(request=request)
 
-    def get_session_id(self, call_id: str) -> str:
-        """
-        get session id by call id
-        """
-        request = voip_pb2.GetSessionIDRequest(
-            call_id=call_id
-        )
-        response: voip_pb2.GetSessionIDResponse = self.voip_stub.GetSessionID(request=request)
-        return response.session_id
+    def get_sip_accounts(
+            self,
+            request: empty_pb2.Empty,
+            context: grpc.ServicerContext,
+    ) -> GetSipAccountsResponse:
+        return self.voip_stub.GetSipAccounts(request=request)
 
-    def shutdown_unhealthy_calls(self) -> bool:
-        """
-        shutdown any deployed call instances with unhealthy overall voip status
-        """
-        request = voip_pb2.ShutdownUnhealthyCallsRequest()
-        response: voip_pb2.ShutdownUnhealthyCallsResponse = self.voip_stub.ShutdownUnhealthyCalls(
-            request=request)
-        return response.success  # type: ignore
+    def get_call_ids_from_sip_account(
+            self,
+            request: GetCallIDsFromSipAccountRequest,
+            context: grpc.ServicerContext,
+    ) -> GetCallIDsFromSipAccountResponse:
+        return self.voip_stub.GetCallIDsFromSipAccount(request=request)
 
-    def get_voip_log(self, call_id: str) -> call_log_pb2.GetVoipLogResponse:
-        """
-        get the call log of a sip-sim instance
-        """
-        request = call_log_pb2.GetVoipLogRequest(call_id=call_id)
-        response: call_log_pb2.GetVoipLogResponse = self.call_log_stub.GetVoipLog(request=request)
-        return response
+    def get_audio_file(
+            self,
+            request: GetAudioFileRequest,
+            context: grpc.ServicerContext,
+    ) -> GetAudioFileResponse:
+        return self.voip_stub.GetAudioFile(request=request)
+
+    def get_full_conversation_audio_file(
+            self,
+            request: GetFullConversationAudioFileRequest,
+            context: grpc.ServicerContext,
+    ) -> GetFullConversationAudioFileResponse:
+        return self.voip_stub.GetFullConversationAudioFile(request=request)
