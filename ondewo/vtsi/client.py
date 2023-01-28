4dbf5318ec2d02005f56b9be7d0d799e22fcb254
Andreas Rath <andreas.rath@ondewo.com>
2023-01-28 18:45:26 +0100

    [OND233-323] Updated client and added init file

--- ondewo/vtsi/client.py
+++ ondewo/vtsi/client.py
@@ -17,7 +17,7 @@ from dataclasses import dataclass
 
 import grpc
 
-from ondewo.vtsi import vtsi_pb2, vtsi_pb2_grpc
+from ondewo.vtsi import projects_pb2, projects_pb2_grpc, calls_pb2_grpc, calls_pb2
 
 
 @dataclass
@@ -61,78 +61,130 @@ class VtsiClient:
         else:
             channel = grpc.insecure_channel(target=target)
             print(f'Creating an insecure channel to {target}')
-        self.vtsi_stub = vtsi_pb2_grpc.VtsiStub(channel=channel)
+        self.projects_stub = projects_pb2_grpc.ProjectsStub(channel=channel)
+        self.calls_stub = calls_pb2_grpc.CallsStub(channel=channel)
 
-    def create_vtsi_project(self, request: vtsi_pb2.CreateVtsiProjectRequest) -> vtsi_pb2.CreateVtsiProjectResponse:
-        return self.vtsi_stub.CreateVtsiProject(request=request)
+    def create_vtsi_project(
+        self,
+        request: projects_pb2.CreateVtsiProjectRequest
+    ) -> projects_pb2.CreateVtsiProjectResponse:
+        return self.projects_stub.CreateVtsiProject(request=request)
 
-    def get_vtsi_project(self, request: vtsi_pb2.GetVtsiProjectRequest) -> vtsi_pb2.VtsiProject:
-        return self.vtsi_stub.GetVtsiProject(request=request)
+    def get_vtsi_project(
+        self,
+        request: projects_pb2.GetVtsiProjectRequest
+    ) -> projects_pb2.VtsiProject:
+        return self.projects_stub.GetVtsiProject(request=request)
 
-    def update_vtsi_project(self, request: vtsi_pb2.UpdateVtsiProjectRequest) -> vtsi_pb2.UpdateVtsiProjectResponse:
-        return self.vtsi_stub.UpdateVtsiProject(request=request)
+    def update_vtsi_project(
+        self,
+        request: projects_pb2.UpdateVtsiProjectRequest
+    ) -> projects_pb2.UpdateVtsiProjectResponse:
+        return self.projects_stub.UpdateVtsiProject(request=request)
 
-    def delete_vtsi_project(self, request: vtsi_pb2.DeleteVtsiProjectRequest) -> vtsi_pb2.DeleteVtsiProjectResponse:
-        return self.vtsi_stub.DeleteVtsiProject(request=request)
+    def delete_vtsi_project(
+        self,
+        request: projects_pb2.DeleteVtsiProjectRequest
+    ) -> projects_pb2.DeleteVtsiProjectResponse:
+        return self.projects_stub.DeleteVtsiProject(request=request)
 
-    def deploy_vtsi_project(self, request: vtsi_pb2.DeployVtsiProjectRequest) -> vtsi_pb2.DeployVtsiProjectResponse:
-        return self.vtsi_stub.DeployVtsiProject(request=request)
+    def deploy_vtsi_project(
+        self,
+        request: projects_pb2.DeployVtsiProjectRequest
+    ) -> projects_pb2.DeployVtsiProjectResponse:
+        return self.projects_stub.DeployVtsiProject(request=request)
 
     def undeploy_vtsi_project(
         self,
-        request: vtsi_pb2.UndeployVtsiProjectRequest
-    ) -> vtsi_pb2.UndeployVtsiProjectResponse:
-        return self.vtsi_stub.UndeployVtsiProject(request=request)
+        request: projects_pb2.UndeployVtsiProjectRequest
+    ) -> projects_pb2.UndeployVtsiProjectResponse:
+        return self.projects_stub.UndeployVtsiProject(request=request)
 
-    def start_caller(self, request: vtsi_pb2.StartCallerRequest) -> vtsi_pb2.StartCallerResponse:
-        return self.vtsi_stub.StartCaller(request=request)
+    def start_caller(
+        self,
+        request: calls_pb2.StartCallerRequest
+    ) -> calls_pb2.StartCallerResponse:
+        return self.calls_stub.StartCaller(request=request)
 
-    def start_callers(self, request: vtsi_pb2.StartCallersRequest) -> vtsi_pb2.StartCallersResponse:
-        return self.vtsi_stub.StartCallers(request=request)
+    def start_callers(
+        self,
+        request: calls_pb2.StartCallersRequest
+    ) -> calls_pb2.StartCallersResponse:
+        return self.calls_stub.StartCallers(request=request)
 
-    def start_listener(self, request: vtsi_pb2.StartListenerRequest) -> vtsi_pb2.StartListenerResponse:
-        return self.vtsi_stub.StartListener(request=request)
+    def start_listener(
+        self,
+        request: calls_pb2.StartListenerRequest
+    ) -> calls_pb2.StartListenerResponse:
+        return self.calls_stub.StartListener(request=request)
 
-    def start_listeners(self, request: vtsi_pb2.StartListenersRequest) -> vtsi_pb2.StartListenersResponse:
-        return self.vtsi_stub.StartListeners(request=request)
+    def start_listeners(
+        self,
+        request: calls_pb2.StartListenersRequest
+    ) -> calls_pb2.StartListenersResponse:
+        return self.calls_stub.StartListeners(request=request)
 
     def start_scheduled_caller(
         self,
-        request: vtsi_pb2.StartScheduledCallerRequest
-    ) -> vtsi_pb2.StartScheduledCallerResponse:
-        return self.vtsi_stub.StartScheduledCaller(request=request)
+        request: calls_pb2.StartScheduledCallerRequest
+    ) -> calls_pb2.StartScheduledCallerResponse:
+        return self.calls_stub.StartScheduledCaller(request=request)
 
     def start_scheduled_callers(
-        self, request: vtsi_pb2.StartScheduledCallersRequest
-    ) -> vtsi_pb2.StartScheduledCallersResponse:
-        return self.vtsi_stub.StartScheduledCallers(request=request)
+        self, request: calls_pb2.StartScheduledCallersRequest
+    ) -> calls_pb2.StartScheduledCallersResponse:
+        return self.calls_stub.StartScheduledCallers(request=request)
 
-    def stop_call(self, request: vtsi_pb2.StopCallRequest) -> vtsi_pb2.StopCallResponse:
-        return self.vtsi_stub.StopCall(request=request)
+    def stop_call(
+        self,
+        request: calls_pb2.StopCallRequest
+    ) -> calls_pb2.StopCallResponse:
+        return self.calls_stub.StopCall(request=request)
 
-    def stop_calls(self, request: vtsi_pb2.StopCallsRequest) -> vtsi_pb2.StopCallsResponse:
-        return self.vtsi_stub.StopCalls(request=request)
+    def stop_calls(
+        self, request:
+        calls_pb2.StopCallsRequest
+    ) -> calls_pb2.StopCallsResponse:
+        return self.calls_stub.StopCalls(request=request)
 
-    def stop_all_calls(self, request: vtsi_pb2.StopAllCallsRequest) -> vtsi_pb2.StopCallsResponse:
-        return self.vtsi_stub.StopAllCalls(request=request)
+    def stop_all_calls(
+        self,
+        request: calls_pb2.StopAllCallsRequest
+    ) -> calls_pb2.StopCallsResponse:
+        return self.calls_stub.StopAllCalls(request=request)
 
-    def transfer_call(self, request: vtsi_pb2.TransferCallRequest) -> vtsi_pb2.TransferCallResponse:
-        return self.vtsi_stub.TransferCall(request=request)
+    def transfer_call(
+        self,
+        request: calls_pb2.TransferCallRequest
+    ) -> calls_pb2.TransferCallResponse:
+        return self.calls_stub.TransferCall(request=request)
 
-    def transfer_calls(self, request: vtsi_pb2.TransferCallsRequest) -> vtsi_pb2.TransferCallsResponse:
-        return self.vtsi_stub.TransferCalls(request=request)
+    def transfer_calls(
+        self,
+        request: calls_pb2.TransferCallsRequest
+    ) -> calls_pb2.TransferCallsResponse:
+        return self.calls_stub.TransferCalls(request=request)
 
-    def get_vtsi_call_info(self, request: vtsi_pb2.GetCallInfoRequest) -> vtsi_pb2.GetCallInfoResponse:
-        return self.vtsi_stub.GetCallInfo(request=request)
+    def get_vtsi_call_info(
+        self,
+        request: calls_pb2.GetCallInfoRequest
+    ) -> calls_pb2.GetCallInfoResponse:
+        return self.calls_stub.GetCallInfo(request=request)
 
-    def list_vtsi_call_info(self, request: vtsi_pb2.ListCallInfoRequest) -> vtsi_pb2.ListCallInfoResponse:
-        return self.vtsi_stub.ListCallInfo(request=request)
+    def list_vtsi_call_info(
+        self,
+        request: calls_pb2.ListCallInfoRequest
+    ) -> calls_pb2.ListCallInfoResponse:
+        return self.calls_stub.ListCallInfo(request=request)
 
-    def get_audio_file(self, request: vtsi_pb2.GetAudioFileRequest) -> vtsi_pb2.GetAudioFileResponse:
-        return self.vtsi_stub.GetAudioFile(request=request)
+    def get_audio_file(
+        self,
+        request: calls_pb2.GetAudioFileRequest
+    ) -> calls_pb2.GetAudioFileResponse:
+        return self.calls_stub.GetAudioFile(request=request)
 
     def get_full_conversation_audio_file(
         self,
-        request: vtsi_pb2.GetFullConversationAudioFileRequest
-    ) -> vtsi_pb2.GetFullConversationAudioFileResponse:
-        return self.vtsi_stub.GetFullConversationAudioFile(request=request)
+        request: calls_pb2.GetFullConversationAudioFileRequest
+    ) -> calls_pb2.GetFullConversationAudioFileResponse:
+        return self.calls_stub.GetFullConversationAudioFile(request=request)
